950c16783e00a812389a871b519fb12b696638d8
denolehov <denolehov@gmail.com>
2024-10-14 22:25:45 +0200

    Baseline tracking

--- src/Phoenix.cpp
+++ src/Phoenix.cpp
@@ -1,16 +1,75 @@
 #include "plugin.hpp"
 
 
+struct BaselineTracker {
+	float current = 1.f; float target = 1.f; float startValue = 1.f;
+	float progress = 0.f;
+
+	float MIN_RECOVERY_SPEED = 0.00001f;
+	float recoverySpeed = MIN_RECOVERY_SPEED;
+
+	float process(const float delta) {
+		if (current == target) {
+			return current;
+		}
+
+		progress += delta;
+		const float p = clamp(progress / recoverySpeed, 0.f, 1.f);
+
+		current = crossfade(startValue, target, easeInAndOut(p));
+
+		if (p >= 1.f) {
+			current = target;
+			progress = 0.f;
+			startValue = current;
+		}
+
+		return current;
+	}
+
+	float getCurrent() const {
+		return current;
+	}
+
+	void weaken(const float strength) {
+		current -= strength;
+		current = clamp(current, 0.f, 1.f);
+		startValue = current;
+		progress = 0.f;
+	}
+
+	void setTarget(const float target) {
+		this->target = target;
+	}
+
+	void setRecoverySpeed(float speed) {
+		recoverySpeed = std::max(speed, MIN_RECOVERY_SPEED);
+	}
+
+	static float easeInAndOut(const float p) {
+		if (p < 0.5) {
+			return 4 * p * p * p;
+		}
+
+		return (p - 1) * (2 * p - 2) * (2 * p - 2) + 1;
+	}
+};
+
+
 struct Phoenix : Module {
 	enum ParamId {
+		FALL_PARAM,
+		RECOVER_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
 		SIGNAL_INPUT,
 		TRIGGER_INPUT,
+		INVERT_INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
+		EOC_OUTPUT,
 		OUT_OUTPUT,
 		OUTPUTS_LEN
 	};
@@ -19,45 +78,52 @@ struct Phoenix : Module {
 	};
 
 	// TODO: Save / Load these
-	float targetBaseline = 0.f;
-	float baseline = 1.f;
+	BaselineTracker baseline;
 
 	dsp::SchmittTrigger trigger;
-	dsp::ExponentialSlewLimiter slewLimiter;
 
 	// TODO: SIGNAL -> MAIN
 	// TODO: OUT -> MAIN
 
 	Phoenix() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(FALL_PARAM, 0.f, 1.f, .1f, "");
+		configParam(RECOVER_PARAM, 0.00001f, 20.f, 0.00001f, "Recover speed", " s");
 		configInput(SIGNAL_INPUT, "");
 		configInput(TRIGGER_INPUT, "");
+		configInput(INVERT_INPUT, "");
+		configOutput(EOC_OUTPUT, "");
 		configOutput(OUT_OUTPUT, "");
 	}
 
 	void process(const ProcessArgs& args) override {
-		// get the input voltage
-		// TODO: Polyphony.
-		// TODO: Configurable input/output ranges (e.g. -5V to 5V, 0V to 10V, etc.)
-		float signal = getInput(SIGNAL_INPUT).getVoltage();
+		float recoverSpeed = getParam(RECOVER_PARAM).getValue();
+		baseline.setRecoverySpeed(recoverSpeed);
 
 		// attenuate it according to the triggers
 		if (trigger.process(getInput(TRIGGER_INPUT).getVoltage(), 0.1f, 2.f)) {
 			// TODO: Add a "slow" mode where the baseline is nudged by an random amount (up to a user-defined limit).
-			baseline -= .1f; // TODO: Make this a parameter
-			baseline = clamp(baseline, 0.f, 1.f);
+			baseline.weaken(getParam(FALL_PARAM).getValue());
 		}
+
+		baseline.process(args.sampleTime);
+
+		// get the input voltage
+		// TODO: Polyphony.
+		// TODO: Configurable input/output ranges (e.g. -5V to 5V, 0V to 10V, etc.)
+		float signal = getInput(SIGNAL_INPUT).getVoltage();
 
 		// TODO: Attenuation mode.
 		// This is for attenuation mode.
 		// float out = signal * baseline;
 
 		// This is for "nudge" (offset) mode.
-		float y_max = rescale(baseline, 0.f, 1.f, -10.f, 10.f);
-		float out = rescale(signal, -10.f, 10.f, -10.f, y_max);
+		const float y_max = rescale(baseline.getCurrent(), 0.f, 1.f, -10.f, 10.f);
+		const float out = rescale(signal, -10.f, 10.f, -10.f, y_max);
 
 		// output it
 		getOutput(OUT_OUTPUT).setVoltage(out);
+		getOutput(EOC_OUTPUT).setVoltage(baseline.getCurrent()); // TODO: This is just for debugging.
 	}
 };
 
@@ -67,15 +133,15 @@ struct PhoenixWidget : ModuleWidget {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/Phoenix.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(7.62, 48.25)), module, Phoenix::FALL_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(7.62, 64.75)), module, Phoenix::RECOVER_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.62, 15.25)), module, Phoenix::SIGNAL_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.62, 31.92)), module, Phoenix::TRIGGER_INPUT));
+		addInput(createInputCentered<DarkPJ301MPort>(mm2px(Vec(7.62, 15.25)), module, Phoenix::SIGNAL_INPUT));
+		addInput(createInputCentered<DarkPJ301MPort>(mm2px(Vec(7.62, 31.92)), module, Phoenix::TRIGGER_INPUT));
+		addInput(createInputCentered<DarkPJ301MPort>(mm2px(Vec(7.62, 94.25)), module, Phoenix::INVERT_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7.62, 48.25)), module, Phoenix::OUT_OUTPUT));
+		addOutput(createOutputCentered<DarkPJ301MPort>(mm2px(Vec(7.62, 80.409)), module, Phoenix::EOC_OUTPUT));
+		addOutput(createOutputCentered<DarkPJ301MPort>(mm2px(Vec(7.62, 110.75)), module, Phoenix::OUT_OUTPUT));
 	}
 };
 
